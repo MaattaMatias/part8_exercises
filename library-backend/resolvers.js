12465e4ff88ba2a24328fe35547d8f63bb57acd8
Matias Maatta <matias.maatta@student.oulu.fi>
2026-05-27 15:53:28 +0300

    13

---
+++ library-backend/resolvers.js
@@ -0,0 +1,142 @@
+import { GraphQLError } from 'graphql'
+import { v1 as uuid } from 'uuid'
+import Author from './models/author.js'
+import Book from './models/book.js'
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "Demons",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+]
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      
+      let result = await Book.find({}).populate('author')
+
+      if(!args.author && !args.genre){
+        return result
+      }
+
+      if(args.author !== undefined){
+        result = result.filter(b => b.author && b.author == args.author)
+      }
+
+      if(args.genre !== undefined){
+        result = result.filter(b => b.genres.includes(args.genre))
+      }
+
+      return result
+      },
+    allAuthors: async () => {
+      return Author.find({})
+    }
+
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      let author = await Author.findOne({name: args.author})
+
+      if(!author){
+        author = new Author({name: args.author})
+      } else{
+        author.bookCount += 1
+      }
+    await author.save()
+    const book = new Book({...args, id: uuid(), author: author})
+    await book.save()
+    return book.populate('author')
+    },
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({name: args.author})
+      if(!author){
+        return null
+      }
+
+      author.born = args.setBornTo
+      return author.save()
+    }
+  },
+  Author: {
+    bookCount: async (root) => {
+      return await Book.countDocuments({author: root._id})
+    }
+  }
+}
+
+export default resolvers
---
+++ library-backend/server.js
@@ -0,0 +1,20 @@
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+
+import resolvers from './resolvers.js'
+import typeDefs from './schema.js'
+
+const startServer = (port) => {
+    const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+startStandaloneServer(server, {
+  listen: { port },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
+}
+
+export default startServer
